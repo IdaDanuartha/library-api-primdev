d45d354cd200a291eb09973ed3f414cf40fbd288
DanDev_ <danuart21@gmail.com>
2026-04-11 11:40:07 +0800

    feat: implement CRUD operations for categories with Prisma

---
+++ controllers/categories.controllers.js
@@ -0,0 +1,130 @@
+import prisma from '../config/database.config.js'
+
+export const getCategories = async (req, res) => {
+  // Mengambil semua buku dari database menggunakan Prisma Client
+  const categories = await prisma.categories.findMany()
+
+  res.json({
+    success: true,
+    message: 'Categories retrieved successfully',
+    data: categories,
+  })
+}
+
+export const getCategoryById = async (req, res) => {
+  // Mendapatkan ID buku yang akan diupdate dari parameter URL
+  // Lalu mengubahnya menjadi tipe data integer menggunakan parseInt
+  const id = parseInt(req.params.id)
+
+  // Mengambil buku dengan ID yang sesuai dari database menggunakan Prisma Client
+  const category = await prisma.categories.findUnique({
+    where: {
+      id: id,
+    },
+  })
+
+  // Jika buku tidak ditemukan, kirimkan pesan error
+  if (!category) {
+    return res.json({
+      success: false,
+      message: `Category with ID: ${id} not found`,
+    })
+  }
+
+  res.json({
+    success: true,
+    message: 'Category retrieved successfully',
+    data: category,
+  })
+}
+
+export const createCategory = async (req, res) => {
+  // Mendapatkan data buku baru dari request body
+  const { name } = req.body
+
+  // Menambahkan buku baru ke database menggunakan Prisma Client
+  const category = await prisma.categories.create({
+    data: {
+      name
+    },
+  })
+
+  res.json({
+    success: true,
+    message: 'Category created successfully',
+    data: category,
+  })
+}
+
+export const updateCategory = async (req, res) => {
+  // Mendapatkan ID buku yang akan diupdate dari parameter URL
+  // Lalu mengubahnya menjadi tipe data integer menggunakan parseInt
+  const id = parseInt(req.params.id)
+
+  // Mendapatkan data buku yang akan diupdate dari request body
+  const { name } = req.body
+
+  // Mencari buku dengan ID yang sesuai di database menggunakan Prisma Client
+  const category = await prisma.categories.findUnique({
+    where: {
+      id: id,
+    },
+  })
+
+  // Jika buku tidak ditemukan, kirimkan pesan error
+  if (!category) {
+    return res.json({
+      success: false,
+      message: `Category with ID: ${id} not found`,
+    })
+  }
+
+  // Mengupdate buku dengan ID yang sesuai di database menggunakan Prisma Client
+  const updatedCategory = await prisma.categories.update({
+    where: {
+      id: id,
+    },
+    data: {
+      name
+    },
+  })
+
+  res.json({
+    success: true,
+    message: 'Category updated successfully',
+    data: updatedCategory,
+  })
+}
+
+export const deleteCategory = async (req, res) => {
+  // Mendapatkan ID buku yang akan diupdate dari parameter URL
+  // Lalu mengubahnya menjadi tipe data integer menggunakan parseInt
+  const id = parseInt(req.params.id)
+
+  // Mencari buku dengan ID yang sesuai di database menggunakan Prisma Client
+  const category = await prisma.categories.findUnique({
+    where: {
+      id: id,
+    },
+  })
+
+  // Jika buku tidak ditemukan, kirimkan pesan error
+  if (!category) {
+    return res.json({
+      success: false,
+      message: `Category with ID: ${id} not found`,
+    })
+  }
+
+  // Menghapus buku dengan ID yang sesuai di database menggunakan Prisma Client
+  await prisma.categories.delete({
+    where: {
+      id: id,
+    },
+  })
+
+  res.json({
+    success: true,
+    message: 'Category deleted successfully',
+  })
+}
--- controllers/index.controllers.js
+++ controllers/index.controllers.js
@@ -1,2 +1,4 @@
 export * from './books.controllers.js'
 export * from './users.controllers.js'
+export * from './profiles.controllers.js'
+export * from './categories.controllers.js'
